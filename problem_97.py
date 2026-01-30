86b84cf56c6d060e63792c9e9dfaba451577a4e0
ravibharathir25it-netizen <ravibharathi.r_25it@kgkite.ac.in>
2026-01-30 19:13:31 +0530

    Update problem_97.py

--- problem_97.py
+++ problem_97.py
@@ -1,15 +1,11 @@
 # Problem 97: Remove element from list
 # Find and fix the error
-
 def remove_element(nums, val):
-    i = 0
-    for j in range(len(nums)):
-        if nums[j] != val:
-            nums[i] = nums[j]
-            i += 1
-    return i
+    i = 0                     # Pointer for the position of the next valid element
+    for j in range(len(nums)): # j scans through all elements
+        if nums[j] != val:    # If current element is not the one to remove
+            nums[i] = nums[j] # Place it at the 'i'-th position
+            i += 1             # Move the position pointer forward
+    return i                  # Return the new length of the array
 
-numbers = [3, 2, 2, 3, 4, 5]
-length = remove_element(numbers, 3)
-print(f"New length: {length}")
-print(f"Modified list: {numbers[:length]}")
+
